457353a2a01047903e197430bac595558d47e0f9
demeritbird <lekjiewei@hotmail.sg>
2023-07-31 14:15:45 +0800

    refactor: remove unnecessary information in parent container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,13 @@
-import {
-  AfterViewInit,
-  Component,
-  ElementRef,
-  HostListener,
-  ViewChild,
-  Renderer2,
-} from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements AfterViewInit {
-  title = 'pointer--demo';
-  markerLeft = 0; // Distance from left of window
-  markerTop = 0; // Distance from top of window.
-  imageLeft = 350; // Initial left of dot from top-left of image.
-  imageTop = 100; // Initial top of dot from top-left of image.
+export class AppComponent {
+  @ViewChild('ide', { static: true }) elementView!: ElementRef;
   imageDimension: Record<any, any> = {};
-
-  scaleHeight: number = 100;
-  scaleWidth: number = 100;
-
   markerArray = [
     {
       x: 100,
@@ -38,45 +23,12 @@ export class AppComponent implements AfterViewInit {
     },
   ];
 
-  @ViewChild('ide', { static: true }) elementView!: ElementRef;
-
-  constructor(public elRef: ElementRef, private renderer: Renderer2) {}
-
-  ngAfterViewInit(): void {
-    this.renderer.listen(this.elementView.nativeElement, 'load', () => {
-      this.initMarker();
-    }); // Set 2nd argument here for delay in rendering, goes well w async stuff.
-  }
-
-  initMarker(): void {
-    this.imageDimension =
-      this.elementView.nativeElement.getBoundingClientRect();
-    // You can access the element reference here and perform any necessary actions.
-
-    this.markerLeft = this.imageDimension['left'] + this.imageLeft;
-    this.markerTop = this.imageDimension['top'] + this.imageTop;
-  }
+  constructor(public elRef: ElementRef) {}
 
   @HostListener('window:resize')
   onWindowResize() {
     this.imageDimension =
       this.elementView.nativeElement.getBoundingClientRect();
     if (!this.imageDimension) return;
-
-    // You can access the element reference here and perform any necessary actions.
-    this.markerLeft = this.imageDimension['left'] + this.imageLeft;
-    this.markerTop = this.imageDimension['top'] + this.imageTop;
-  }
-
-  handleClick(event: MouseEvent): void {
-    // const imageElement = event.target as HTMLImageElement;
-    //const imageElement = document.querySelector('.image') as HTMLImageElement;
-    const imageElement = document.getElementById('ide') as HTMLImageElement;
-    // const rect = imageElement.getBoundingClientRect();
-    const xPosition = event.clientX;
-    const yPosition = event.clientY;
-
-    this.markerLeft = xPosition;
-    this.markerTop = yPosition;
   }
 }
