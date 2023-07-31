a878fd27d95c3c5ffc2d9c4e2a9002e419293970
demeritbird <lekjiewei@hotmail.sg>
2023-07-31 17:35:22 +0800

    feat: update pointers on zoom image via scroll

--- src/app/marker-point/marker-point.component.ts
+++ src/app/marker-point/marker-point.component.ts
@@ -21,6 +21,7 @@ export class MarkerPointComponent implements AfterViewInit, OnChanges {
   @Input() containerView!: ElementRef;
   @Input() container: any;
 
+  @Input() scaleFactor: number = 1;
   @Input() imageLeft: number = 0; // Distance from left of image to point
   @Input() imageTop: number = 0; // Distance from top of image to point
   @Input() markerLeft: number = 0; // Distance from left of window to point
@@ -33,38 +34,24 @@ export class MarkerPointComponent implements AfterViewInit, OnChanges {
       this.initMarker();
     });
   }
-  initMarker(): void {
-    // You can access the element reference here and perform any necessary actions.
+
+  updateMarkerPosition(): void {
     this.image = this.imageView.nativeElement.getBoundingClientRect();
     this.container = this.containerView.nativeElement.getBoundingClientRect();
 
-    this.markerLeft = this.image.left + this.imageLeft;
-    this.markerTop = this.image.top + this.imageTop;
+    this.markerLeft = this.image.left + this.imageLeft * this.scaleFactor;
+    this.markerTop = this.image.top + this.imageTop * this.scaleFactor;
 
     console.log('marker', { left: this.markerLeft, top: this.markerTop });
     console.log('image', { left: this.imageLeft, top: this.imageTop });
+  }
+
+  initMarker(): void {
+    // You can access the element reference here and perform any necessary actions.
+    this.updateMarkerPosition();
   }
 
   ngOnChanges(): void {
-    this.image = this.imageView.nativeElement.getBoundingClientRect();
-    this.container = this.containerView.nativeElement.getBoundingClientRect();
-
-    this.markerLeft = this.image.left + this.imageLeft;
-    this.markerTop = this.image.top + this.imageTop;
-
-    console.log('marker', { left: this.markerLeft, top: this.markerTop });
-    console.log('image', { left: this.imageLeft, top: this.imageTop });
-  }
-
-  @HostListener('window:resize')
-  onWindowResize() {
-    if (this.image) {
-      // You can access the element reference here and perform any necessary actions.
-      this.markerLeft = this.image.left + this.imageLeft;
-      this.markerTop = this.image.top + this.imageTop;
-    }
-
-    console.log('marker', { left: this.markerLeft, top: this.markerTop });
-    console.log('image', { left: this.imageLeft, top: this.imageTop });
+    this.updateMarkerPosition();
   }
 }
